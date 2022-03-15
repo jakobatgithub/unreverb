debb42ad9c31405471cdccd7e18e4a87d63028de
jakobatgithub <jakob@physik.tu-berlin.de>
2022-03-15 12:56:59 +0100

    Fixed typos

--- GenerateImpulseResponseFunctions.py
+++ GenerateImpulseResponseFunctions.py
@@ -4,7 +4,7 @@ import soundfile as sf
 import rir_generator as rir
 
 # A script to generate the Impulse Response (IR) Functions of a rectangular room
-# of a given size with given receiever and source positions.
+# of a given size with given receiver and source positions and given Reverberation time T60.
 
 # https://github.com/audiolabs/rir-generator
 # pip install rir-generator
@@ -25,12 +25,13 @@ for reverberation_time in reverberation_times:
     	],
     	s=[0.5, 2.5, 1.5],          # Source position [x y z] (m)
     	L=[5, 5, 3],            # Room dimensions [x y z] (m)
-    	reverberation_time=reverberation_time, # Reverberation time (s)
+    	reverberation_time=reverberation_time, # Reverberation time T60 (s)
     	nsample=samplerate,           # Number of output samples
 	)
     print(IR_data.shape)              # (4096, 3)
     sf.write('random_IRs/IRF_'+str(idx)+'.wav', IR_data, samplerate)
     idx = idx +1	
+
 #print(signal.shape)         # (11462, 2)
 
 # Convolve 2-channel signal with 3 impulse responses
